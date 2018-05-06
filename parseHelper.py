cbe08537356378d8ac81ad6f42eadcd5803132b1
imlocle <loc.tan.le@gmail.com>
2018-05-06 15:14:38 -0700

    comment for parse_defintitions

--- parseHelper.py
+++ parseHelper.py
@@ -1,6 +1,11 @@
 import re
 
-# Using Regex to scrape
+
+"""
+This function is to parse out content using a regular expression pattern from a string.
+<param>regex_pattern</param>
+<param>string</param>
+"""
 def parse_definition(regex_pattern, string):
     i = re.compile(regex_pattern, flags=re.MULTILINE|re.DOTALL)
     return i.search(string).group(1)
